f127806984ad2a644668a0925e9b19501ac3d213
Yakov Shmidt <yakovshmidt93@gmail.com>
2021-06-14 22:48:52 +0600

    Initial commit

---
+++ src/test/kotlin/kz/home/pomodoroapigateway/PomodoroApiGatewayApplicationTests.kt
@@ -0,0 +1,13 @@
+package kz.home.pomodoroapigateway
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PomodoroApiGatewayApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
